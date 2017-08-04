bfefb3e74318e594a25db4a008c43c8cdc7822bd
VincentCastig <vincent.castig@gmail.com>
2017-08-04 12:34:03 -0500

    part 1

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -9,7 +9,6 @@
 
 function daBears(){
   var isFurry = true;
-cv
   function papaBear (){
     var porridge = "Too Hot!";
     var chair = "Too Big!";
@@ -43,27 +42,27 @@ cv
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear", "mamaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -73,9 +72,12 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // Write a constructor function called Vehicle.  Vehicle should have a property
 // called gasRemaining that is equal to 100.
 
+
 // Next, assign a function called drive to the Vehicle prototype.  When invoked,
 // drive should subtract 25 from the gasRemaining property of any Vehicle your constructor
 // function creates.
+
+Vehicle()
 
 // Create 2 new Vehicles with the constructor function you made: one called "charger",
 // the other called "mustang".  Using implicit context, invoke the drive method on
@@ -83,8 +85,20 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function Vehicle (){
+  this.gasRemaining =  100;
+}
+Vehicle.prototype.drive = function(){
+  this.gasRemaining -= 25
+}
 
-
+var charger = new Vehicle
+var mustang = new Vehicle
+console.log(mustang)
+charger.drive()
+mustang.drive()
+mustang.drive()
+console.log(mustang)
 
 
 // -----------------------------------------------------------------------------
@@ -108,6 +122,27 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 
 // CODE HERE...
+String.prototype.grammarPolice = function() {
+  var upper = ""
+  var swap = true
+
+  for(char of this){
+    if(swap){
+      upper += char.toUpperCase()
+      swap = false;
+    }
+    else {
+      upper += char.toLowerCase()
+    }
+
+    if(char === ' '){
+      swap = true;
+    }
+  }
+
+ return upper
+}
+
 
 
 
@@ -127,6 +162,18 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function valueType(a,b){
+  if(a === b){
+    return "Exactly the same"
+  }
+  else if(a == b){
+    return "Same value, different types"
+  }
+  else {
+    return "Different values"
+  }
+}
+
 
 
 // *************
@@ -141,3 +188,9 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 var theAnswer = "Unknown";
 
 // CODE HERE...
+
+function promiseCatcher(a){
+  a.then(function(response){
+    theAnswer = response
+  })
+}
